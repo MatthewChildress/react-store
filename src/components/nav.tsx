450728be8505d3b56958f3961a70f0ec868bb6ba
matthew.childress <matthew.childress@bisonline.com>
2022-07-12 14:24:04 -0400

    starter nav function component created

---
+++ src/components/nav.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    text?:string;
+    id?: number;
+
+}
+
+export const Nav: React.FC<Props> = () => {
+  return (
+    <nav>
+    </nav>
+  )
+};
+
